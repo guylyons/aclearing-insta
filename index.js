0f45fb2eebbb6090eb291115b31f19ee625b176b
Guy Lyons <glyons1@une.edu>
2020-01-31 15:46:16 -0500

    remove comment

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/ig', function (req, res) {
   ig.scrapeUserPage('aclearing.maine').then(result => {
       res.send(result);
